201d013087267582ceb040323d40af54ccf09172
Cassey Anene <casseyanene@gmail.com>
2021-03-19 23:15:19 -0500

    created the form

--- src/ProfilePage/Form.js
+++ src/ProfilePage/Form.js
@@ -1,6 +1,61 @@
 import React from "react";
 
 
-export default function Form() {
-    return <h2>Form</h2>;
+export default function ProfileForm() {
+    return (
+
+        <div className="wrapper">
+        <h1>Your Information</h1>
+        <form>
+        <fieldset>
+        <label>
+           <p>Name</p>
+           <input name="name" />
+         </label>
+
+         <label>
+           <p>Email Address</p>
+           <input name="email" />
+         </label>
+
+         <label>
+           <p>Phone number</p>
+           <input name="number" />
+         </label>
+
+         <label>
+           <p>Graduation Date</p>
+           <select name="month" >
+               <option value="">--Please choose an option--</option>
+               <option value="Jan">January</option>
+           </select>
+
+           <select name="year" >
+               <option value="">--Please choose an option--</option>
+               <option value="2022">2022</option>
+           </select>
+           </label>
+
+
+        <label>
+           <p>Work Experience</p>
+           <input name="workExp" />
+         </label>
+
+         <label>
+           <p>Skills</p>
+           <select name="skills" >
+               <option value="">--Please choose an option--</option>
+               <option value="css">CSS</option>
+               <option value="python">Python</option>
+           </select>
+           </label>
+           </fieldset>
+           </form>
+        <button type="submit"> Update Information </button>
+        </div>
+    );
+
+        
+
 }
