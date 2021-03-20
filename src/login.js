a989eee2a3f4344b30b9d4fbc202a16db4e6c9f7
Bradon Thymes <bmt.wcpa@gmail.com>
2021-03-20 00:07:42 -0400

    Added functionality to Login Screen

--- src/login.js
+++ src/login.js
@@ -1,6 +1,28 @@
 import React from "react";
+import { Button } from '@material-ui/core'
 
+class AppLoginButton extends React.Component{
+  render(){
+      return(
+        <Button>Applicant Login</Button>
+      )
+  };
+}
+
+class RecLoginButton extends React.Component{
+  render(){
+    return(
+      <Button>Recruiter Login</Button>
+    )
+};
+}
 
 export default function Login() {
-    return <h2>Login</h2>;
+    return (
+      <div>
+        <h1>evn</h1>
+        <AppLoginButton/>
+        <RecLoginButton/>
+      </div>
+    );
   }
