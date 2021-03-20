00e3fa12979d42a937c70d2102add6c56baba64e
Ciara Wharton <whartonciara1299@gmail.com>
2021-03-20 11:52:11 -0400

    adding "sidebar"

--- src/ApplicantPage/Apphome.js
+++ src/ApplicantPage/Apphome.js
@@ -3,12 +3,6 @@ import Header from "../Header";
 import Card from "../Card";
 import SideBar from "../Sidebar";
 
-
-
-
-
-
-
 export default function AppHome() {
   const cells = [];
   const randJobs = ["Software Engineering Intern", "Backend Intern", "iOS Developer"]
@@ -21,6 +15,23 @@ export default function AppHome() {
     return (
     <div classname="app">
     <Header logged="true" />
+    <div className="sidebar">
+            <div className="sidebarTop">
+            <img className="evn_logo"src="https://www.billionsinstitute.com/wp-content/uploads/2014/10/Jennifer-Circle-Headshot-300X300.png"></img>
+                <h2>Jane Doe</h2>
+                <h7> &nbsp;</h7>
+                <h4>Recruiter for Spotify</h4>
+            </div>
+            
+            <div className="sidebarStats">
+                <div className="sidebarStat">
+                    <p>Candidate Matches</p>
+                    <p className="sidebarStatNum">20</p>
+                </div>
+                <div className="sidebarStat">Your Job Listings</div>
+                <p className="sidebarStatNum">14</p>
+            </div>
+        </div>
     {cells}
     </div>
     );
