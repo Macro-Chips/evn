e7c852207074f0e63c0de4fe9453f47da5b7e1c4
Bradon Thymes <bmt.wcpa@gmail.com>
2021-03-20 02:41:37 -0400

    Added some stuff

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <nav className="header">
+            <img className="evn_logo"src="https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo1.png"></img>
+            
+        </nav>
+    )
+}
+
+export default Header
--- src/RecruiterPage/Rechome.js
+++ src/RecruiterPage/Rechome.js
@@ -3,4 +3,7 @@ import React from "react";
 
 export default function RecHome() {
     return <h2>Recruiter Home/ Candidates</h2>;
+    {/* Header */}
+    {/* App Body */}
+      {/* Side Bar */}
   }
